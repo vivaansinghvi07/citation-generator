719129b68e5e13f105144cc425206a5d9f74628f
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-02-28 19:37:39 -0500

    combined into one function

--- removes.py
+++ removes.py
@@ -1,35 +1,21 @@
 import re
 
-# removes anything between script tags in the data
-def removeScripts(data):
-    # gets all occurences of "<script..." in the data
-    starts = [m.start() for m in re.finditer('<script', data)][::-1]
+# removes everything between html tags labeled "target"
+def remove(data, target):
+    # gets all occurences of "<target..." in the data
+    starts = [m.start() for m in re.finditer('<' + target, data)][::-1]
 
-    # gets all occurences of "</script" in the data
-    stops = [m.start() for m in re.finditer('</script', data)][::-1]
+    # gets all occurences of "</target" in the data
+    stops = [m.start() for m in re.finditer('</' + target, data)][::-1]
 
     # both are reversed in order to delete properly
 
-    # performs removals
+    # converts length to variable to decrease runtime
+    length = len(target)
+
+    # performs removals; 3 added to account for the </ and > in the end tag
     for start, stop in zip(starts, stops): 
-        data = data[:start] + data[(stop + 9):]
+        data = data[:start] + data[(stop + length + 3):]
 
     # returns the data
-    return data
-
-# removes anything between style tags in the data
-def removeStyles(data):
-    # gets all occurences of "<style..." in the data
-    starts = [m.start() for m in re.finditer('<style', data)][::-1]
-
-    # gets all occurences of "</style..." in the data
-    stops = [m.start() for m in re.finditer('</style', data)][::-1]
-
-    # reversed for same reason
-
-    # goes through every and removes all styles
-    for start, stop in zip(starts, stops):
-        data = data[:start] + data[(stop + 8):]
-
-    # returns data
     return data
